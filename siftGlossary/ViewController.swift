c3c551f16ff3deae73382c374bfba935517ac8a7
Mario <marioperozo@gmail.com>
2020-04-26 15:47:28 -0400

    Initial Commit

---
+++ siftGlossary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  siftGlossary
+//
+//  Created by Mario Perozo on 4/26/20.
+//  Copyright © 2020 Mario Perozo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
